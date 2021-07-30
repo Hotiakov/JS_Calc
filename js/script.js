17306472aeb0373fbabfe0bae8beb98d8ea467d1
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-07-30 11:01:43 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 50205;
+let income = "инвестиции";
+let addExpenses = "Проезд в метро, Коммуналка, Интернет, Продукты питания";
+let deposit = true;
+let mission = 151515;
+let period = 8;
 
-alert("Это мой первый урок по JS от Glo-Academy!");
+let budgetDay = money / 30;
 
-console.log("Уверен, будет круто!");
+console.log("Тип переменной money: " + typeof money);
+console.log("Тип переменной income: " + typeof income);
+console.log("Тип переменной deposit: " + typeof deposit);
+
+console.log("Длина строки addExpenses: " + addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase().split(", "));
+
+console.log("Дневной доход составляет: " + budgetDay + " рублей");
+
